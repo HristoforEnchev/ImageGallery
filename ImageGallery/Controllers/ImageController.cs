f07ab2d9b0508a68c0b0de6ca1758bf51590e6cd
HristoforEnchev <35043525+HristoforEnchev@users.noreply.github.com>
2018-01-05 13:56:55 +0200

    Upload Details Form

---
+++ ImageGallery/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿namespace ImageGallery.Controllers
+{
+    using ImageGallery.Models.Image;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(ImageUploadModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ ImageGallery/Models/Image/ImageUploadModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageGallery.Models.Image
+{
+    using Microsoft.AspNetCore.Http;
+
+    public class ImageUploadModel
+    {
+        public string Title { get; set; }
+
+        public string Tags { get; set; }
+
+        public IFormFile FormFile { get; set; }
+    }
+}
